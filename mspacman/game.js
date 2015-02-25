43065af29a1a47ced04d9fe8398bd3e7cf4609e2
hannahcclark <hannah_clark@mac.com>
2015-02-25 16:46:44 -0500

    Draws ms. pacman on board

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,11 +1,10 @@
-//size needs to be 462 x 140
 function init() {
 	var canvas = document.getElementById("game_canvas");
 	var context = canvas.getContext('2d');
 	var spriteSheet = new Image();
 	spriteSheet.onload = function(){
 		context.drawImage(spriteSheet, 324, 0, 462, 140, 0, 0, 462, 140);
-		//canvas.drawImage(spriteSheet, sx, sy, 18, 18, 0, 0 462, 140);
+		context.drawImage(spriteSheet, 83, 23, 15, 15, 35, 33, 15, 15);
 	};
 	spriteSheet.src = 'pacman10-hp-sprite.png';
 }
