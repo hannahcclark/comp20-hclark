74032245cebec9c4f3f71616ab8b2fb0f9f1e4f7
hannahcclark <hannah_clark@mac.com>
2015-03-07 13:49:33 -0500

    JSON parsed and added to HTML content

--- messages/lab.js
+++ messages/lab.js
@@ -2,9 +2,12 @@ function parse () {
 	request = new XMLHttpRequest();
 	request.onreadystatechange = function () {
 		if (request.readyState == 4 && request.status == 200) {
-			console.log(request.response);
+			request.response.forEach(function (val, i, arr) {
+				messagesDiv = document.getElementById('messages');
+				messagesDiv.innerHTML += '<div class="message"><p class="content">' + val['content'] + '</p><p class="sender">' + val['username'] + '</p></div>' ;
+			})
 		}
-		console.log(request.readyState);
+
 	};
 	request.responseType = "json"
 	request.open("get", "data.json", true);
