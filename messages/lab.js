d42e08d41570bd45f19c71f7837f2c818143ccbf
hannahcclark <hannah_clark@mac.com>
2015-03-07 10:15:00 -0500

    fixed readyState typo

--- messages/lab.js
+++ messages/lab.js
@@ -1,10 +1,10 @@
 function parse () {
 	request = new XMLHttpRequest();
 	request.onreadystatechange = function () {
-		if (request.readystate == 4 && request.status == 200) {
+		if (request.readyState == 4 && request.status == 200) {
 			console.log(request.response);
 		}
-		console.log(request.readystate);
+		console.log(request.readyState);
 	};
 	request.responseType = "json"
 	request.open("get", "data.json", true);
