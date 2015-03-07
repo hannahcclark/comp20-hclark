531c4162e3a378e03860d3a1bc6356198fe613dc
hannahcclark <hannah_clark@mac.com>
2015-03-07 10:03:44 -0500

    created request

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here...
+function writeMessages () {
+	if (this.readystate == 4 && this.response.status == 200) {
+		console.log(this.responseText);
+	};
+}
+
+function parse () {
+	request = new XMLHttpRequest();
+	request.onreadystatechange = writeMessages;
+	request.responseType = "json"
+	request.open("get", "messages.json", true);
+	request.send();
+}
+
